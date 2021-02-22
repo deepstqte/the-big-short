e9ceb6ea29ddce6eb42591c68b1ae00b9db3bcac
Hamza BENHMANI <hamza@plot.ly>
2021-02-22 09:52:42 -0500

    Add optimization function

--- utils_bigshort.py
+++ utils_bigshort.py
@@ -79,3 +79,28 @@ def str_catencoder(df, method_switch=10):
             return_df[column] = return_df[column].map(str)
             return_df[column] = le.fit_transform(return_df[column])
     return return_df
+
+def optimize_numtypes(dataframe):
+    np_types = [np.int8 ,np.int16 ,np.int32, np.int64,
+               np.uint8 ,np.uint16, np.uint32, np.uint64]
+    np_types = [np_type.__name__ for np_type in np_types]
+    type_df = pd.DataFrame(data=np_types, columns=['class_type'])
+    type_df['min_value'] = type_df['class_type'].apply(lambda row: np.iinfo(row).min)
+    type_df['max_value'] = type_df['class_type'].apply(lambda row: np.iinfo(row).max)
+    type_df['range'] = type_df['max_value'] - type_df['min_value']
+    type_df.sort_values(by='range', inplace=True)
+    for col in dataframe.loc[:, dataframe.dtypes <= np.int64]:
+        col_min = dataframe[col].min()
+        col_max = dataframe[col].max()
+        temp = type_df[(type_df['min_value'] <= col_min) & (type_df['max_value'] >= col_max)]
+        optimized_class = temp.loc[temp['range'].idxmin(), 'class_type']
+        print("Col name : {} Col min_value : {} Col max_value : {} Optimized Class : {}".format(col, col_min, col_max, optimized_class))
+        dataframe[col] = dataframe[col].astype(optimized_class)
+    for col in dataframe.loc[:, dataframe.dtypes == np.float64]:
+        col_min = dataframe[col].min()
+        col_max = dataframe[col].max()
+        temp = floattype_df[(floattype_df['min_value'] <= col_min) & (floattype_df['max_value'] >= col_max)]
+        optimized_class = temp.loc[temp['range'].idxmin(), 'class_type']
+        print("Col name : {} Col min_value : {} Col max_value : {} Optimized Class : {}".format(col, col_min, col_max, optimized_class))
+        dataframe[col] = dataframe[col].astype(optimized_class)
+    return dataframe
